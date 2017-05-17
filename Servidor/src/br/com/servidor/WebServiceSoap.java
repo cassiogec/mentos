859efc61f87cd5a3bcc68ccddede5bfec8cfec7c
Cássio Eckstein <cassiogec@gmail.com>
2017-05-16 23:50:04 -0300

    Ajustes para WebService utilizar o DAO

--- Servidor/src/br/com/servidor/WebServiceSoap.java
+++ Servidor/src/br/com/servidor/WebServiceSoap.java
@@ -1,48 +1,81 @@
 package br.com.servidor;
 
-import java.util.Date;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
+import br.com.DAO.veiculoDAO;
+import br.com.negocio.Veiculo;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
 
-@WebService
-@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
-public class WebServiceSoap{
+@WebService(endpointInterface = "br.com.servidor.WebServiceSoapInterface")
+public class WebServiceSoap implements WebServiceSoapInterface{
     
-    @WebMethod
     public Boolean adicionarVeiculo(String dsPlaca,
-                                    Integer idTipo, 
+                                    Integer idTipo,
                                     Integer vlCapacidade,
                                     String dsUnidade) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Veiculo v = new Veiculo(dsPlaca, idTipo, vlCapacidade, dsUnidade);
+            veiculoDAO vdao = new veiculoDAO();
+            vdao.incluir(v);
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(WebServiceSoap.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
 
-    @WebMethod
-    public Boolean alterarVeiculo(String dsPlaca, 
-                                  Integer idTipo, 
+    public Boolean alterarVeiculo(Integer cdVeiculo,
+                                  String dsPlaca,
+                                  Integer idTipo,
                                   Integer vlCapacidade,
                                   String dsUnidade) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Veiculo v = new Veiculo(cdVeiculo, dsPlaca, idTipo, vlCapacidade, dsUnidade);
+            veiculoDAO vdao = new veiculoDAO();
+            vdao.alterar(v);
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(WebServiceSoap.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
 
-    @WebMethod
-    public Boolean excluirVeiculo(String dsPlaca) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Boolean excluirVeiculo(Integer cdVeiculo) {
+        try {
+            Veiculo v = new Veiculo(cdVeiculo);
+            veiculoDAO vdao = new veiculoDAO();
+            vdao.excluir(v);
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(WebServiceSoap.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
 
-    @WebMethod
-    public Boolean consultarVeiculo(String dsPlaca) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Veiculo consultarVeiculo(Integer cdVeiculo) throws Exception{
+        try {
+            veiculoDAO vdao = new veiculoDAO();
+            Veiculo v = vdao.consultarVeiculo(cdVeiculo);
+            return v;
+        } catch (Exception ex) {
+            Logger.getLogger(WebServiceSoap.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception();
+        }
     }
     
-    @WebMethod
-    public Boolean listaTipo(Integer idTipo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Veiculo> listaTipo(Integer idTipo) throws Exception{
+        try {
+            veiculoDAO vdao = new veiculoDAO();
+            List<Veiculo> list = vdao.consultarVeiculosPorTipo(idTipo);
+            return list;
+        } catch (Exception ex) {
+            Logger.getLogger(WebServiceSoap.class.getName()).log(Level.SEVERE, null, ex);
+            throw new Exception();
+        }
     }
 
-    @WebMethod
-    public Boolean localizacao(String dsPlaca, Date dtInicial) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Integer localizacao() {
+        return 1;
     }
 }
