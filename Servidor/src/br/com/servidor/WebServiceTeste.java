bcb7e99cfa79d8cdbf089f5985874ff43c412aed
Cássio Eckstein <cassiogec@gmail.com>
2017-05-24 23:43:05 -0300

    Alterado projeto para utilização do GlassFish

--- Servidor/src/br/com/servidor/WebServiceTeste.java
+++ Servidor/src/br/com/servidor/WebServiceTeste.java
@@ -13,17 +13,21 @@ public class WebServiceTeste {
 
     public static void main(String args[]) {
         try {
+            System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+            System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+            System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
+            System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dumpTreshold", "999999");
             URL url = new URL("http://127.0.0.1:9876/Servidor/WebServiceSoap?wsdl");
             QName qname = new QName("http://servidor.com.br/",
                                     "WebServiceSoapService");
             Service ws = Service.create(url, qname);
-
+            
             WebServiceSoapInterface soap = ws.getPort(WebServiceSoapInterface.class);
-        
-//            System.out.println(soap.adicionarVeiculo("AAA1113", 0, 1000, "TESTE"));
+            
+            System.out.println(soap.adicionarVeiculo("AAA1113", 0, 1000, "TESTE"));
 //            System.out.println(soap.alterarVeiculo(1, "AAA1111", 1, 666, "TEST2"));
             
-              System.out.println(soap.listaTipo(1));
+              System.out.println(soap.listaTipo(0));
 //            System.out.println(soap.excluirVeiculo(5));
         } catch(Exception e)
         {
