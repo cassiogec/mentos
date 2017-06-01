3252a4d80e5c88e3e61b8a90a1af1f3fbc3a4c6c
Cássio Eckstein <cassiogec@gmail.com>
2017-06-01 13:35:40 -0300

    Ajustado Threads TCPService

--- Cliente/Java/TCPClient/src/TCPCliente/TCPClient.java
+++ Cliente/Java/TCPClient/src/TCPCliente/TCPClient.java
@@ -3,6 +3,7 @@ package TCPCliente;
 import br.com.negocio.Veiculo;
 import br.com.negocio.ArquivoBD;
 import br.com.negocio.Posicao;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
@@ -16,6 +17,8 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TCPClient {
 
@@ -126,195 +129,201 @@ public class TCPClient {
         return (ArquivoBD) inp.readObject();
     }
     
-    public static void main(String[] args) throws Exception{
-        Scanner leitor = new Scanner(System.in);
-                
-        int porta = 2010;
-        
-        Socket s = new Socket("localhost", porta);
-        System.out.println("Cliente conectado ao servidor na porta " + porta + "\n");
-
-        while (true)
-        {
-            System.out.println("1 - Adicionar Veiculo");
-            System.out.println("2 - Alterar Veiculo");
-            System.out.println("3 - Excluir Veiculo");
-            System.out.println("4 - Consultar Veiculo");
-            System.out.println("5 - Listar veiculos por tipo");
-            System.out.println("6 - Localização do Veiculo");
-            System.out.println("7 - Sair e fechar conexão");
-            System.out.println("Digite o código da operação: ");
-            Integer operacao = Integer.parseInt(leitor.nextLine());
-
-            Veiculo v = new Veiculo();
-            ArquivoBD arquivo = new ArquivoBD();
-            List<Object> l = new ArrayList<Object>();
-
-            switch (operacao)
+    public static void main(String[] args){
+        try {
+            Scanner leitor = new Scanner(System.in);
+            
+            int porta = 2010;
+            
+            Socket s = new Socket("localhost", porta);
+            System.out.println("Cliente conectado ao servidor na porta " + porta + "\n");
+            
+            while (true)
             {
-                case 1:
-                    v = lerVeiculo();
-                    l.add(v);
-
-                    arquivo.setOpe(operacao);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-                    
-                    if (arquivo.getCode() == 0)
-                    {
-                        System.out.println(arquivo.getRetorno());
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                break;
-
-                case 2:
-                    v = lerVeiculo();
-
-                    arquivo.setOpe(operacao);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-                    
-                    if (arquivo.getCode() == 0)
-                    {
-                        System.out.println(arquivo.getRetorno());
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                break;
-
-                case 3:
-                    System.out.println("Digite a placa do veiculo, sem formatação: ");
-                    v.setPlaca(leitor.nextLine());
-
-                    l.add(v);
-                    arquivo.setOpe(operacao);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-                    
-                    if (arquivo.getCode() == 0)
-                    {
-                        System.out.println(arquivo.getRetorno());
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                break;
-
-                case 4:
-                    System.out.println("Digite a placa do veiculo, sem formatação: ");
-                    v.setPlaca(leitor.nextLine());
-
-                    l.add(v);
-                    arquivo.setOpe(operacao);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-                    
-                    if (arquivo.getCode() == 0)
-                    {
-                        v = (Veiculo) arquivo.getObjetos().get(0);
+                System.out.println("1 - Adicionar Veiculo");
+                System.out.println("2 - Alterar Veiculo");
+                System.out.println("3 - Excluir Veiculo");
+                System.out.println("4 - Consultar Veiculo");
+                System.out.println("5 - Listar veiculos por tipo");
+                System.out.println("6 - Localização do Veiculo");
+                System.out.println("7 - Sair e fechar conexão");
+                System.out.println("Digite o código da operação: ");
+                Integer operacao = Integer.parseInt(leitor.nextLine());
+                
+                Veiculo v = new Veiculo();
+                ArquivoBD arquivo = new ArquivoBD();
+                List<Object> l = new ArrayList<Object>();
+                
+                switch (operacao)
+                {
+                    case 1:
+                        v = lerVeiculo();
+                        l.add(v);
                         
-                        System.out.println("Placa: " + v.getPlaca());
-                        System.out.println("Capacidade: " + v.getCapacidade());
-                        System.out.println("Tipo: " + formataTipo(v.getTipo()));
-                        System.out.println("Unidade: " + v.getUncapac());
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                    
-                break;
-
-                case 5:
-                    v.setTipo(lerTipo());
-
-                    l.add(v);
-                    arquivo.setOpe(operacao);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-                    
-                    if (arquivo.getCode() == 0)
-                    {
-                        System.out.println("Tipo: " + formataTipo(v.getTipo()));
-                        System.out.println("-----------------------------------------------------------");
+                        arquivo.setOpe(operacao);
+                        arquivo.setObjetos(l);
                         
-                        for (Object vei : arquivo.getObjetos())
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
                         {
-                            v = (Veiculo) vei;
-
-                            System.out.print("Placa: " + v.getPlaca());
-                            System.out.print(" Capacidade: " + v.getCapacidade());
-                            System.out.print(" Tipo: " + formataTipo(v.getTipo()));
-                            System.out.print(" Unidade: " + v.getUncapac());
-                            System.out.println("-----------------------------------------------------------");
+                            System.out.println(arquivo.getRetorno());
                         }
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                break;
-
-                case 6:
-                    System.out.println("Digite a placa do veiculo, sem formatação: ");
-                    v.setPlaca(leitor.nextLine());
-
-                    System.out.println("Digite a data da localização(Formato DD/MM/YYYY HH:MM:SS), ou deixe-a em branco para pegar todas as localizações");
-                    String data = leitor.nextLine();
-
-                    if (!data.equals(""))
-                    {
-                        Calendar cal = Calendar.getInstance();
-                        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss", Locale.ENGLISH);
-                        cal.setTime(sdf.parse(data));
-                        arquivo.setData(cal);
-                    }
-
-                    l.add(v);
-                    arquivo.setObjetos(l);
-
-                    arquivo = comunicar(s, arquivo);
-
-                    if (arquivo.getCode() == 0)
-                    {
-                        System.out.println("Veiculo: " + v.getPlaca());
-                        System.out.println("-----------------------------------------------------------");
-                        
-                        for (Object pos : arquivo.getObjetos())
+                        else if (arquivo.getCode() == 1)
                         {
-                            Posicao p = (Posicao) pos;
-
-                            System.out.print("Data: " + p.getDatahora().toString());
-                            System.out.print(" Latitude: " + p.getLatitude());
-                            System.out.print(" Longitude: " + p.getLongitude());
-                            System.out.println("-----------------------------------------------------------");
+                            System.out.println("Erro: " + arquivo.getRetorno());
                         }
-                    }
-                    else if (arquivo.getCode() == 1)
-                    {
-                        System.out.println("Erro: " + arquivo.getRetorno());
-                    }
-                break;
-
-                case 7:
-                    s.close();
-                    System.exit(0);
-                break;
-                default:
-                    System.out.println("Operação Inválida");
-                break;
+                        break;
+                        
+                    case 2:
+                        v = lerVeiculo();
+                        
+                        arquivo.setOpe(operacao);
+                        arquivo.setObjetos(l);
+                        
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
+                        {
+                            System.out.println(arquivo.getRetorno());
+                        }
+                        else if (arquivo.getCode() == 1)
+                        {
+                            System.out.println("Erro: " + arquivo.getRetorno());
+                        }
+                        break;
+                        
+                    case 3:
+                        System.out.println("Digite a placa do veiculo, sem formatação: ");
+                        v.setPlaca(leitor.nextLine());
+                        
+                        l.add(v);
+                        arquivo.setOpe(operacao);
+                        arquivo.setObjetos(l);
+                        
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
+                        {
+                            System.out.println(arquivo.getRetorno());
+                        }
+                        else if (arquivo.getCode() == 1)
+                        {
+                            System.out.println("Erro: " + arquivo.getRetorno());
+                        }
+                        break;
+                        
+                    case 4:
+                        System.out.println("Digite a placa do veiculo, sem formatação: ");
+                        v.setPlaca(leitor.nextLine());
+                        
+                        l.add(v);
+                        arquivo.setOpe(operacao);
+                        arquivo.setObjetos(l);
+                        
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
+                        {
+                            v = (Veiculo) arquivo.getObjetos().get(0);
+                            
+                            System.out.println("Placa: " + v.getPlaca());
+                            System.out.println("Capacidade: " + v.getCapacidade());
+                            System.out.println("Tipo: " + formataTipo(v.getTipo()));
+                            System.out.println("Unidade: " + v.getUncapac());
+                        }
+                        else if (arquivo.getCode() == 1)
+                        {
+                            System.out.println("Erro: " + arquivo.getRetorno());
+                        }
+                        
+                        break;
+                        
+                    case 5:
+                        v.setTipo(lerTipo());
+                        
+                        l.add(v);
+                        arquivo.setOpe(operacao);
+                        arquivo.setObjetos(l);
+                        
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
+                        {
+                            System.out.println("Tipo: " + formataTipo(v.getTipo()));
+                            System.out.println("-----------------------------------------------------------");
+                            
+                            for (Object vei : arquivo.getObjetos())
+                            {
+                                v = (Veiculo) vei;
+                                
+                                System.out.print("Placa: " + v.getPlaca());
+                                System.out.print(" Capacidade: " + v.getCapacidade());
+                                System.out.print(" Tipo: " + formataTipo(v.getTipo()));
+                                System.out.print(" Unidade: " + v.getUncapac());
+                                System.out.println("-----------------------------------------------------------");
+                            }
+                        }
+                        else if (arquivo.getCode() == 1)
+                        {
+                            System.out.println("Erro: " + arquivo.getRetorno());
+                        }
+                        break;
+                        
+                    case 6:
+                        System.out.println("Digite a placa do veiculo, sem formatação: ");
+                        v.setPlaca(leitor.nextLine());
+                        
+                        System.out.println("Digite a data da localização(Formato DD/MM/YYYY HH:MM:SS), ou deixe-a em branco para pegar todas as localizações");
+                        String data = leitor.nextLine();
+                        
+                        if (!data.equals(""))
+                        {
+                            Calendar cal = Calendar.getInstance();
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss", Locale.ENGLISH);
+                            cal.setTime(sdf.parse(data));
+                            arquivo.setData(cal);
+                        }
+                        
+                        l.add(v);
+                        arquivo.setObjetos(l);
+                        
+                        arquivo = comunicar(s, arquivo);
+                        
+                        if (arquivo.getCode() == 0)
+                        {
+                            System.out.println("Veiculo: " + v.getPlaca());
+                            System.out.println("-----------------------------------------------------------");
+                            
+                            for (Object pos : arquivo.getObjetos())
+                            {
+                                Posicao p = (Posicao) pos;
+                                
+                                System.out.print("Data: " + p.getDatahora().toString());
+                                System.out.print(" Latitude: " + p.getLatitude());
+                                System.out.print(" Longitude: " + p.getLongitude());
+                                System.out.println("-----------------------------------------------------------");
+                            }
+                        }
+                        else if (arquivo.getCode() == 1)
+                        {
+                            System.out.println("Erro: " + arquivo.getRetorno());
+                        }
+                        break;
+                        
+                    case 7:
+                        s.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Operação Inválida");
+                        break;
+                }
             }
+        } catch (IOException ex) {
+            Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }    
 }
